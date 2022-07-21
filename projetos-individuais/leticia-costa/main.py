8800cb9ceea0afed438ff5af37c0802eac0fe275
thatianecoelho <107770087+thatianecoelho@users.noreply.github.com>
2022-07-21 10:42:10 -0300

    thati-certificado

--- projetos-individuais/leticia-costa/main.py
+++
@@ -1,8 +0,0 @@
-titulo = 'hello world, my friend'
-numero_de_palavras = len(titulo.split())
-mostrar_variaveis = True
-
-if mostrar_variaveis:
-    print(titulo, numero_de_palavras)
-else:
-    print('as variáveis não serão mostradas')
